783b2cfa139e9a5a3c1261a61f98c1824cf7f770
KaszkaManna <m.kowalewski@outlook.com>
2020-11-08 20:33:26 +0100

    create framebuff

--- main.cpp
+++ main.cpp
@@ -70,6 +70,8 @@ class HelloTriangle
 
 	std::vector<VkImage> swapChainImages;
 	std::vector<VkImageView> swapChainImageViews;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+
 	VkFormat swapChainImageFormat{};
 	VkExtent2D swapChainExtent{};
 
@@ -680,6 +682,29 @@ class HelloTriangle
 		vkDestroyShaderModule(device, fragShaderModule, nullptr);
 	}
 
+	void createFramebuffers()
+	{
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+
+		for(size_t i = 0; i < swapChainImageViews.size(); i++)
+		{
+			VkImageView attachments[] = {swapChainImageViews[i]};
+		
+			VkFramebufferCreateInfo framebufferInfo{};
+
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			framebufferInfo.renderPass = renderPass;
+			framebufferInfo.attachmentCount = 1;
+			framebufferInfo.pAttachments = attachments;
+			framebufferInfo.width = swapChainExtent.width;
+			framebufferInfo.height = swapChainExtent.height;
+			framebufferInfo.layers = 1;
+
+			if(vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS)
+				throw std::runtime_error("Failed to create framebuffer!");
+		}
+	}
+
 	void initVulkan()
 	{
 		createInstance();
@@ -691,6 +716,7 @@ class HelloTriangle
 		createImageViews();
 		createRenderPass();
 		createGraphicsPipeline();
+		createFramebuffers();
 	}
 
 	void mainLoop()
@@ -701,6 +727,9 @@ class HelloTriangle
 
 	void cleanup()
 	{
+		for(auto& framebuffer : swapChainFramebuffers)
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+		
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
