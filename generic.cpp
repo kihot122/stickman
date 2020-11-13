99857e801e9e8ffc760c246fe9eb78951371636e
klubson <mateuszk3899@wp.pl>
2020-11-13 19:05:04 +0100

    generic-class-2

--- generic.cpp
+++ generic.cpp
@@ -1,27 +1,19 @@
 #include "generic.h"
 #include <iostream>
 
-Base::Base(){
-
+Base::Base(std::vector<b2BodyDef> vec){
+    handler_container = vec;
 };
 
-int Base::findElement(void* element){
-    int index = -2;
-    for(int i = 0; i < handler_container.size(); i++){
-        if(handler_container[i] == element){
-            index = i;
-        }
-    }
-    if (element < 0){
-        std::cout<<"brak takiego elementu"<< std::endl;
-    }
-    else return index;
-}
+virtual void tick(){
 
-void* Base::getElement(void* element){
-    return handler_container[findElement(element)];
-}
+};
+virtual int getFeedback(){
 
-void Base::addElement(void* element){
-    handler_container.insert(handler_container.begin(), element);
-}
+};
+virtual std::vector<uint8_t>* serialize()= 0 {
+
+};
+virtual b2BodyDef deserialize()= 0 {
+
+};
