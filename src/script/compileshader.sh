446e0444f48419dad34b49a61ff19805cf679dba
Michal Kowalewski <m.kowalewski@outlook.com>
2020-12-05 21:00:17 +0100

    [script] quickfix

--- src/script/compileshader.sh
+++ src/script/compileshader.sh
@@ -1,2 +1,3 @@
+mkdir data
 glslc -O --target-env=vulkan1.2 -fshader-stage=vert src/shader/vertex.glsl -o data/vertex.spv
 glslc -O --target-env=vulkan1.2 -fshader-stage=frag src/shader/fragment.glsl -o data/fragment.spv
